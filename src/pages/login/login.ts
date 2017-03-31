153f05e66a01840503e35aedf6654eba77fed1f4
Huan LI <zixia@zixia.net>
2017-04-01 01:13:48 +1300

    login(github): adding github login support

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core'
-import { NavController } from 'ionic-angular'
+import { Component }      from '@angular/core'
+import {
+  Auth,
+  User,
+}                         from '@ionic/cloud-angular'
+import { NavController }  from 'ionic-angular'
+
+import { Brolog }         from 'brolog'
 
 /*
-  Generated class for the Login page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
+  https://docs.ionic.io/services/auth/github-auth.html
 */
 @Component({
   selector: 'page-login',
@@ -14,13 +17,46 @@ import { NavController } from 'ionic-angular'
 export class LoginPage {
 
   constructor(
-    public navCtrl: NavController,
+    private auth:     Auth,
+    private log:      Brolog,
+    private navCtrl:  NavController,
+    private user:     User,
   ) {
+    this.log.verbose('LoginPage', 'constructor()')
 
   }
 
   ionViewDidLoad() {
-    console.log('Hello LoginPage Page')
+    this.log.verbose('LoginPage', 'ionViewDidLoad()')
   }
+
+  async login() {
+    this.log.verbose('LoginPage', 'login()')
+    try {
+      const authLoginResult = await this.auth.login('github')
+      if (authLoginResult.signup) {
+        this.log.verbose('LoginPage', 'login() new user signup for %s'
+                                    , this.user.social.github.uid
+                        )
+      } else {
+        this.log.verbose('LoginPage', 'login() returned user login for %s'
+                                    , this.user.social.github.uid
+                        )
+      }
+
+      this.log.silly('LoginPage', 'login() %s'
+                                , JSON.stringify(this.user.social.github.data)
+                    )
+
+    } catch (e) {
+      this.log.error('LoginPage', 'login() %s', e.message)
+    }
+  }
+
+  logout() {
+    this.log.verbose('LoginPage', 'logout()')
+    this.auth.logout()
+  }
+
 
 }
