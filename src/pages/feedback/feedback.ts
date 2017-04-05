2a3bf51488e2ecffe6ba49c19902a96cb2bf27c7
Huan LI <zixia@zixia.net>
2017-04-06 10:08:14 +1200

    add Feedback Page

---
+++ src/pages/feedback/feedback.ts
@@ -0,0 +1,27 @@
+import { Component }  from '@angular/core'
+import {
+  NavController,
+  NavParams
+}                     from 'ionic-angular'
+
+import { Brolog }     from 'brolog'
+
+@Component({
+  selector: 'page-feedback',
+  templateUrl: 'feedback.html',
+})
+export class FeedbackPage {
+
+  constructor(
+    public log:       Brolog,
+    public navCtrl:   NavController,
+    public navParams: NavParams,
+  ) {
+    this.log.verbose('FeedbackPage', 'constructor()')
+  }
+
+  ionViewDidLoad() {
+    this.log.verbose('FeedbackPage', 'ionViewDidLoad()')
+  }
+
+}
