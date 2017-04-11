5897f367614262b423f5acc8409c88a6b8699acb
Huan LI <zixia@zixia.net>
2017-04-11 14:15:49 +0800

    output apk with version in filename

--- script/android.sh
+++ script/android.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+VERSION=$(jq -r .version < package.json)
+
 KEYSTORE='akamobi.keystore'
 APK_RELEASE_UNSIGNED='platforms/android/build/outputs/apk/android-release-unsigned.apk'
-APK_RELEASE_SIGNED='chatie.apk'
+APK_RELEASE_SIGNED="chatie-$VERSION.apk"
 
 ionic build android --release
 
@@ -11,6 +13,7 @@ jarsigner -verbose \
   -sigalg SHA1withRSA \
   -digestalg SHA1 \
   -keystore "$KEYSTORE" \
+  -storepass "$ANDROID_SDK_KEYSTORE_PASSWORD_CHATIE" \
   "$APK_RELEASE_UNSIGNED" \
   release
 
