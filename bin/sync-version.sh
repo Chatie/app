6b8a50441272626ef6d2c8dd2f1b0309c5d9a0e6
Huan LI <zixia@zixia.net>
2017-04-11 11:45:51 +0800

    test sync version

--- bin/sync-version.sh
+++ bin/sync-version.sh
@@ -8,11 +8,15 @@ PATCH_NEXT=$(($PATCH+1))
 
 VERSION_NEXT="$MAJOR_MINOR.$PATCH_NEXT"
 
-echo "$VERSION -> $VERSION_NEXT"
-
 #
 # Sync the App version to package.json
 #
 sed -i \
   '/"io.chatie.app" version="/s/version="[^"]*"/version="'"$VERSION_NEXT"'"/' \
   config.xml
+
+MSG="bump(version): $VERSION -> $VERSION_NEXT"
+echo "$MSG"
+
+git add config.xml
+git commit -m "$MSG"
