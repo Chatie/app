eeb2e138ba501e1dbed301c7deecfd75aad774a2
Huan LI <zixia@zixia.net>
2017-04-11 16:55:56 +0800

    prototype ionic deploy

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,7 +1,10 @@
 import { Component }  from '@angular/core'
+import { Deploy }     from '@ionic/cloud-angular'
 import {
+  AlertController,
   NavController,
   NavParams,
+  ToastController,
 }                     from 'ionic-angular'
 
 import { Brolog }     from 'brolog'
@@ -17,11 +20,15 @@ const packageJson = require('../../package.json')
 })
 export class AboutPage {
   version: string = packageJson.version
+  clickCounter = 0
 
   constructor(
+    public alertCtrl: AlertController,
+    public deploy:    Deploy,
     public log:       Brolog,
     public navCtrl:   NavController,
     public navParams: NavParams,
+    public toastCtrl: ToastController,
   ) {
     this.log.verbose('AboutPage', 'constructor()')
   }
@@ -37,4 +44,57 @@ export class AboutPage {
   status() {
     this.navCtrl.push(StatusPage)
   }
+
+  async checkDeploy() {
+    this.log.verbose('AboutPage', 'check() clickCounter=%s', this.clickCounter)
+
+    if (this.clickCounter <= 3) {
+      const toast = this.toastCtrl.create({
+        message: (3 - this.clickCounter).toString(),
+        duration: 500,
+        position: 'middle',
+      })
+      toast.onDidDismiss(() => this.clickCounter++)
+      toast.present()
+      return
+    }
+
+    this.deploy.channel = 'dev'
+
+    try {
+      const hasUpdate = await this.deploy.check()
+      if (hasUpdate) {
+        this.log.silly('AboutPage', 'checkDeploy() found new update!')
+
+        const metaData = this.deploy.getMetadata()
+        this.log.silly('AboutPage', 'check() metaData of update: %s', JSON.stringify(metaData))
+
+        this.log.silly('AboutPage', 'check() downloading...')
+        await this.deploy.download()
+
+        this.log.silly('AboutPage', 'check() extracting...')
+        await this.deploy.extract()
+
+        const snapshotList = await this.deploy.getSnapshots() as string[]
+        this.log.silly('AboutPage', 'check() we has %s snapshots: %s',
+                                    snapshotList.length,
+                                    snapshotList.join(',')
+                      )
+
+        this.log.silly('AboutPage', 'check() loading...')
+        this.deploy.load()
+
+      }
+
+    } catch (e) {
+      this.log.warn('AboutPage', 'check() exception: %s', e.message)
+
+      this.alertCtrl.create({
+        title:    'Check Error',
+        subTitle: 'Exception: ' + e.message,
+        buttons:  ['OK'],
+      }).present()
+    }
+  }
+
 }
