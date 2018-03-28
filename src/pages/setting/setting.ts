74e9cdcf38b36a2292be6d97d7265c3e33e48635
Huan LI <zixia@zixia.net>
2018-03-28 14:49:50 +0800

    add Pro Deploy Update Feature

--- src/pages/setting/setting.ts
+++ src/pages/setting/setting.ts
@@ -22,12 +22,17 @@ import { LogoutPage } from '../logout/'
 export class SettingPage {
   public notificate = true
 
+  public deployChannel    = ''
+  public isBeta           = false
+  public downloadProgress = 0
+
   constructor(
     public log:       Brolog,
     public navCtrl:   NavController,
     public navParams: NavParams,
   ) {
     this.log.verbose('SettingPage', 'constructor()')
+    this.checkChannel()
   }
 
   public ionViewDidLoad() {
@@ -66,6 +71,100 @@ export class SettingPage {
       newFn()
     } catch (e) {
       console.log('call wrap func error still be throwed to outside')
+    }
+
+  }
+
+  /**
+   * https://ionicframework.com/docs/pro/deploy/plugin-api.html
+   */
+  public async checkChannel() {
+    try {
+      const res = await Pro.deploy.info()
+      this.deployChannel = res.channel || 'unknown(web?)'
+      this.isBeta = (this.deployChannel === 'Beta')
+    } catch (err) {
+      // We encountered an error.
+      // Here's how we would log it to Ionic Pro Monitoring while also catching:
+
+      Pro.monitoring.exception(err)
+    }
+  }
+
+  public async toggleBeta() {
+    const config = {
+      channel: (this.isBeta ? 'Beta' : 'Production')
+    }
+
+    try {
+      await Pro.deploy.init(config)
+      await this.checkChannel()
+      await this.performAutomaticUpdate() // Alternatively, to customize how this works, use performManualUpdate()
+    } catch (err) {
+      // We encountered an error.
+      // Here's how we would log it to Ionic Pro Monitoring while also catching:
+
+      Pro.monitoring.exception(err)
+    }
+
+  }
+
+  public async performAutomaticUpdate() {
+
+    /*
+      This code performs an entire Check, Download, Extract, Redirect flow for
+      you so you don't have to program the entire flow yourself. This should
+      work for a majority of use cases.
+    */
+
+    try {
+      const resp = await Pro.deploy.checkAndApply(true, progress => {
+          this.downloadProgress = progress
+      })
+
+      if (resp.update) {
+        // We found an update, and are in process of redirecting you since you put true!
+      } else {
+        // No update available
+      }
+    } catch (err) {
+      // We encountered an error.
+      // Here's how we would log it to Ionic Pro Monitoring while also catching:
+
+      Pro.monitoring.exception(err)
+    }
+  }
+
+  public async performManualUpdate() {
+
+    /*
+      Here we are going through each manual step of the update process:
+      Check, Download, Extract, and Redirect.
+      This code is currently exactly the same as performAutomaticUpdate,
+      but you could split it out to customize the flow.
+
+      Ex: Check, Download, Extract when a user logs into your app,
+        but Redirect when they logout for an app that is always running
+        but used with multiple users (like at a doctors office).
+    */
+
+    try {
+      const haveUpdate = await Pro.deploy.check()
+
+      if (haveUpdate) {
+        this.downloadProgress = 0
+
+        await Pro.deploy.download(progress => {
+          this.downloadProgress = progress
+        })
+        await Pro.deploy.extract()
+        await Pro.deploy.redirect()
+      }
+    } catch (err) {
+      // We encountered an error.
+      // Here's how we would log it to Ionic Pro Monitoring while also catching:
+
+      Pro.monitoring.exception(err)
     }
 
   }
